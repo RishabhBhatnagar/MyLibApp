57902a5eb87f278fad58268137e44058ce98fd89
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-06-27 09:44:28 +0530

    Create different loading screen for logged in user
    If not logged in, login form is shown

--- app/src/main/java/com/example/vinay/mylibapp/LoginActivity.java
+++ app/src/main/java/com/example/vinay/mylibapp/LoginActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,8 +30,10 @@ public class LoginActivity extends AppCompatActivity implements MyCallback{
     TextView tv_result_login;
     String pid;
     String pwd;
-    
 
+
+    // Make this true, to run on flask server
+    // If testing=true, then accordingly change urls in the DataHolder constructor
     DataHolder dataHolder = new DataHolder(false);
 
 
@@ -46,26 +49,14 @@ public class LoginActivity extends AppCompatActivity implements MyCallback{
     final String KEY_PID = "pid";
     final String KEY_PWD = "pwd";
 
+    // In the full_screen_loading layout
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
-        // Create SharedPreferences
-        sharedPreferences = getApplicationContext().getSharedPreferences(titleSharedPrefs, MODE_PRIVATE);
-        pid = sharedPreferences.getString(KEY_PID, null);
-        pwd = sharedPreferences.getString(KEY_PWD, null);
-
-        if(pid != null && pwd != null){
-            // There exists previous user
-
-            GoGoGadget goGoGadget = new GoGoGadget((MyCallback) LoginActivity.this,
-                    dataHolder.getBundleURLs(),
-                    GoGoGadget.LOGIN_AND_GET_COOKIES,
-                    handler);
-            new Thread(goGoGadget).start();
-        }
+        //setContentView(R.layout.activity_login);
+        // set ContentView is done later, depending on wheter previous logged in user exists
 
         // Create a loadingDialog instance for the activity to show during network operations
         alertDialogBuilder = new AlertDialog.Builder(this);
@@ -75,33 +66,65 @@ public class LoginActivity extends AppCompatActivity implements MyCallback{
         loadingDialog.setCanceledOnTouchOutside(false);
 
 
-        et_pid = findViewById(R.id.et_pid);
-        et_pwd = findViewById(R.id.et_pwd);
-        btn_login_submit = findViewById(R.id.btn_login_submit);
-        tv_result_login = findViewById(R.id.tv_login_result);
+        // Create SharedPreferences
+        sharedPreferences = getApplicationContext().getSharedPreferences(titleSharedPrefs, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.apply();
+        pid = sharedPreferences.getString(KEY_PID, null);
+        pwd = sharedPreferences.getString(KEY_PWD, null);
+
+        if(pid != null && pwd != null){
+            //region Previous logged in user exists
+
+            // Then set loading screen as background
+            setContentView(R.layout.full_screen_loading);
+
+            progressBar = findViewById(R.id.network_progress);
+
+            GoGoGadget goGoGadget = new GoGoGadget((MyCallback) LoginActivity.this,
+                    dataHolder.getBundleURLs(),
+                    GoGoGadget.LOGIN_AND_GET_COOKIES,
+                    handler);
+            new Thread(goGoGadget).start();
+            //endregion
+        }
+        else {
+            //region No Logged in user
+
+            // If there is no logged in user,
+            // show the login form
+            setContentView(R.layout.activity_login);
+
+            et_pid = findViewById(R.id.et_pid);
+            et_pwd = findViewById(R.id.et_pwd);
+            btn_login_submit = findViewById(R.id.btn_login_submit);
+            tv_result_login = findViewById(R.id.tv_login_result);
+
+            // TODO: Debug
+            btn_login_submit.setText("BBBB");
+            // if button has this text, even after incorrect login, then its working correct
+
+            //region btn_login_submit OnClickListener
+            btn_login_submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    setLoadingDialog(true);
+
+                    pid = et_pid.getText().toString();
+                    pwd = et_pwd.getText().toString();
 
 
-
-
-        //region btn_login_submit OnClickListener
-        btn_login_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                setLoadingDialog(true);
-
-                pid = et_pid.getText().toString();
-                pwd = et_pwd.getText().toString();
-
-
-                final GoGoGadget goGoGadget = new GoGoGadget((MyCallback) LoginActivity.this,
-                        dataHolder.getBundleURLs(),
-                        GoGoGadget.LOGIN_AND_GET_COOKIES,
-                        handler);
-                new Thread(goGoGadget).start();
-            }
-        });
-        //endregion
+                    final GoGoGadget goGoGadget = new GoGoGadget((MyCallback) LoginActivity.this,
+                            dataHolder.getBundleURLs(),
+                            GoGoGadget.LOGIN_AND_GET_COOKIES,
+                            handler);
+                    new Thread(goGoGadget).start();
+                }
+            });
+            //endregion
+            //endregion
+        }//else
     }
 
     @Override
@@ -113,9 +136,18 @@ public class LoginActivity extends AppCompatActivity implements MyCallback{
     public void sendStudentNameToCaller(String name) {
         // This method will be called when login is correct
 
+        // Do this only in the full_screen_loading layout, ie when it's not null
+        // Make the progressbar disappear
+        if(progressBar != null)
+            progressBar.setVisibility(View.GONE);
+
+        // Add the correct pid/pwd to shared prefs
+        editor.putString(KEY_PID, pid);
+        editor.putString(KEY_PWD, pwd);
+        editor.apply();
 
         setLoadingDialog(false);
-        tv_result_login.setText("SUCCESS LOGIN");
+//        tv_result_login.setText("SUCCESS LOGIN");
 
         // Go to MainActivity
         final AlertDialog loginSuccessDialog = new AlertDialog.Builder(
@@ -134,10 +166,38 @@ public class LoginActivity extends AppCompatActivity implements MyCallback{
     @Override
     public void passErrorsToCaller(int errorCode) {
         setLoadingDialog(false);
-        tv_result_login.setText("Some error happened"+ errorCode);
+//        tv_result_login.setText("Some error happened"+ errorCode);
 
         switch (errorCode){
             case ERROR_INCORRECT_PID_OR_PASSWORD:
+
+                // Clear incorrect pid/pwd if present
+                // TODO: Maybe change editor.clear() to making null for those specific keys
+                editor.clear();
+                editor.apply();
+
+
+                // Create AlertDialog for login failure
+                AlertDialog loginFailedDialog = new AlertDialog.Builder(this).create();
+                loginFailedDialog.setTitle("Login Failed!");
+                loginFailedDialog.setMessage("Incorrect PID or password");
+                loginFailedDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+
+                        // Restart activity
+                        recreate();
+                        // Now, since we have cleared the shared prefs,
+                        // The activity will have no previous user
+                        // It will now go into the block for no previous user
+                    }
+                });
+
+                // Show the dialog
+                loginFailedDialog.show();
+                break;
+            //TODO: Other errors' handling remains
 
         }
     }
