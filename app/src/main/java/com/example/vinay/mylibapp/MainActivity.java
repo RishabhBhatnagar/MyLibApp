2f0e05c299dad681ca5a3fd91ed19cf733d2f28b
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-06-27 14:47:52 +0530

    Create button for nav drawer

--- app/src/main/java/com/example/vinay/mylibapp/MainActivity.java
+++ app/src/main/java/com/example/vinay/mylibapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.vinay.mylibapp;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
         // Set a Toolbar to replace the ActionBar
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
 
         // Find our drawer view
         mDrawerLayout = findViewById(R.id.drawer_layout);
@@ -44,6 +51,16 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
